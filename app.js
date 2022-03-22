a3673bac14fd5031dafa38112264b9122ab6a6b8
Ashley Smith <ashlynn4567@gmail.com>
2022-03-22 16:45:53 -0500

    testing node.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // console.log("Hello Node!");
 
+
 // var message = "Hello Node!";
-
 // var sum = 5 + 3;
-
 // console.log(message);
 // console.log(sum);
 
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
