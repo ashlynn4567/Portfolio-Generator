e1e26835a309c1fc309a55545d69c59e957c475f
Ashley Smith <ashlynn4567@gmail.com>
2022-03-22 16:45:10 -0500

    testing node.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Hello Node!");
+
+// var message = "Hello Node!";
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
